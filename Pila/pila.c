c4ee7b43ed05ae6219a92b202240ccc662f1d024
Erick Castañeda Martínez <erickce40@gmail.com>
2019-11-25 22:37:40 -0600

    creacion del makefile de la pila

--- Pila/pila.c
+++ Pila/pila.c
@@ -1,28 +1,18 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "limits.h"
-#include "errno.h"
-
-struct nodo{
-  int dato;
-  struct nodo *siguiente;
-};
-
-
-void push(struct nodo **mipila, int num);
-int pop(struct nodo **mipila);
-int top(struct nodo *mipila);
-void mostrarpila(struct nodo **mipila);
+#include "pila.h"
 
 int main(void) {
 
-  struct nodo *mipila = NULL;
+  struct Pila *mipila = NULL;
 
   push(&mipila, 10);
   push(&mipila, 20);
   push(&mipila, 30);
   push(&mipila, 40);
   push(&mipila, 50);
+  push(&mipila, 60);
+  push(&mipila, 70);
+  push(&mipila, 80);
+  push(&mipila, 90);
 
   printf("El elemento eliminado es: %d\n", pop(&mipila));
   printf("El elemento eliminado es: %d\n", pop(&mipila));
@@ -35,59 +25,4 @@ int main(void) {
   mostrarpila(&mipila);
 
   return 0;
-}
-
-void push(struct nodo **mipila, int num){
-
-  struct nodo *temporal = malloc(sizeof(struct nodo));
-  temporal -> dato = num;
-  temporal -> siguiente = *mipila;
-
-  if(mipila == NULL){
-    return;
-  }
-
-  if(*mipila == NULL){
-    *mipila = temporal;
-    return;
-  } else {
-
-    *mipila = temporal;
-
-  }
-
-}
-
-int pop(struct nodo **mipila){
-
-  if(*mipila == NULL){
-    exit(1);
-  }
-
-  int valor = (**mipila).dato;
-
-  struct nodo *temporal = *mipila;
-
-  *mipila = temporal -> siguiente;
-
-  free(temporal);
-
-
-  return valor;
-}
-
-int top(struct nodo *mipila){
-
-  if(mipila == NULL){
-    exit(1);
-  }
-
-  int valor = (*mipila).dato;
-  return valor;
-}
-
-void mostrarpila(struct nodo **mipila){
-  while(mipila != NULL){
-    printf("%d\n", pop(mipila) );
-  }
 }
